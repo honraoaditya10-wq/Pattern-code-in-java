30538c16dacb7198106b546f40c2ecb76c9b5bb3
Aditya Honrao <honraoaditya10@gmail.com>
2026-01-05 22:51:24 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,28 @@
+import java.util.*;
+/* 
+
+       *
+      ***
+     *****
+    *******
+
+*/
+
+public class pattern1{
+    public static void main(String[] args){
+
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=5-i; j++){
+                if(i<j &&  ){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
